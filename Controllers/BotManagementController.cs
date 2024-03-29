218ec7efd1a9b4858c80114b8782f9f808680f8c
mohsen <71mohsenpsh@gmail.com>
2024-03-29 04:04:01 +0330

    add buttons

--- Controllers/BotManagementController.cs
+++ Controllers/BotManagementController.cs
@@ -1,20 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using TeckNews.Entities;
+using TeckNews.Repositories;
+using TeckNews.Utilities;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using User = TeckNews.Entities.User;
 
-namespace FarzamNews.Controllers
+namespace TeckNews.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class BotManagementController : ControllerBase
     {
         private readonly TelegramBotClient _bot;
+        private readonly IBaseRepository<User> _userRepository;
 
-        public BotManagementController() 
+        public BotManagementController(IBaseRepository<User> userRepository)
         {
             _bot = new TelegramBotClient("6026122963:AAFivrrnCEbaXa8ZacZZR2SwO5KDlM4vfHI");
+            _userRepository = userRepository;
         }
 
         [HttpGet("[action]")]
@@ -31,16 +39,82 @@ namespace FarzamNews.Controllers
         }
 
         [HttpPost("[action]")]
-        public async Task<ActionResult> ReceiveUpdate(object model)
+        public async Task<ActionResult> ReceiveUpdate(object model, CancellationToken cancellationToken)
         {
             var update = JsonConvert.DeserializeObject<Update>(model.ToString());
 
-            if(update.Message != null)
-            {
+            if (update == null)
+                return Ok();
 
+            if (update.Message != null)
+            {
+                var text = update.Message.Text;
+                var chatId = update.Message.Chat.Id;
+
+                var user = await _userRepository.TableNoTracking.FirstOrDefaultAsync(x => x.ChatId == chatId);
+
+                if (user == null)
+                {
+                    user = await _userRepository.AddAsync(new User()
+                    {
+                        ChatId = chatId,
+                        FirstName = update.Message.From.FirstName,
+                        LastName = update.Message.From.LastName,
+                        Username = update.Message.From.Username,
+                        UserType = UserType.Guest,
+                        ParentId = null
+                    }, cancellationToken);
+                }
+
+                if (text == DefaultContents.Start)
+                {
+                    await _bot.SendTextMessageAsync(chatId, DefaultContents.WelcomeToBot, replyMarkup: GenerateMainKeyboard());
+                }
+                else if (text == DefaultContents.Location)
+                {
+                    await _bot.SendVenueAsync(chatId, 45.87654, 56.76543, "دفتر مرکزی", "خیابان ایکس پلاک 2");
+                }
+                else if (text == DefaultContents.ContactUs)
+                {
+                    await _bot.SendTextMessageAsync(chatId, DefaultContents.ContactUsMessage);
+                }
+                else if (text == DefaultContents.Money)
+                {
+                    await _bot.SendTextMessageAsync(chatId, DefaultContents.MoneyMessage);
+                }
             }
 
             return Ok();
+        }
+
+        private ReplyKeyboardMarkup GenerateMainKeyboard()
+        {
+            var rows = new List<KeyboardButton[]>();
+
+            rows.Add(new KeyboardButton[]
+            {
+                new KeyboardButton(DefaultContents.Search)
+            });
+
+            rows.Add(new KeyboardButton[]
+            {
+                new KeyboardButton(DefaultContents.HeadOfNews),
+                new KeyboardButton(DefaultContents.Money)
+            });
+
+            rows.Add(new KeyboardButton[]
+            {
+                new KeyboardButton(DefaultContents.Location),
+                new KeyboardButton(DefaultContents.ContactUs)
+            });
+
+            rows.Add(new KeyboardButton[]
+            {
+                new KeyboardButton(DefaultContents.Profile)
+            });
+
+            var keyboard = new ReplyKeyboardMarkup(rows);
+            return keyboard;
         }
     }
 }
--- Utilities/DefaultContents.cs
+++ Utilities/DefaultContents.cs
@@ -5,5 +5,15 @@ namespace TeckNews.Utilities
     public static class DefaultContents
     {
         public static readonly string Start = "/start".CleanString();
+        public static readonly string WelcomeToBot = "به ربات خوش امدید".CleanString();
+        public static readonly string Search = "\U0001F50E جستجو \U0001F50E".CleanString();
+        public static readonly string HeadOfNews = "\U0001F4F0 سرتیتر اخبار \U0001F4F0".CleanString();
+        public static readonly string Money = "\U0001F4C8 قیمت ارز \U0001F4C8".CleanString();
+        public static readonly string Location = "\U0001F4CC آدرس دفتر مرکزی \U0001F4CC".CleanString();
+        public static readonly string ContactUs = "\U0001F4E7 پل های ارتباطی \U0001F4E7".CleanString();
+        public static readonly string Profile = "\U0001F464 پروفایل \U0001F464".CleanString();
+
+        public static readonly string ContactUsMessage = "\U0001F4E7: f_yamini72@yahoo.com\n\U0000260E: 09215488280".CleanString();
+        public static readonly string MoneyMessage = "دلار: 50,000 تومان\nیورو:53,000".CleanString();
     }
 }
